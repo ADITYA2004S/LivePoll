f0192e6f395a744157b7d7cb72a76771a39a9a8f
Aditya Singh <adityasinghofficial06@gmail.com>
2025-06-01 13:21:54 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -48,9 +48,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  // });
 } else {
   app.use(express.static("public"));
 }
